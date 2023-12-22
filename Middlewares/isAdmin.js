197e07cc9521586406b3527717aebc73a4b02b55
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-22 19:48:28 +0330

    Feat : Create isAdmin Middleware

--- Middlewares/isAdmin.js
+++ Middlewares/isAdmin.js
@@ -1,3 +1,6 @@
 export default isAdmin = async (req, res, next) => {
-
+    if (req.user.role === 'ADMIN') {
+        return next();
+    }
+    res.status(422).json({ message: "This Route Access For Admins Only" });
 }
