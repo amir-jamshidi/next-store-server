591fe8c140041d40efff195eb35fd864fc023103
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-31 15:28:52 +0330

    Feat : Create Cart Model

--- Router/Auth.js
+++ Router/Auth.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getMe, login, register, validation } from "../Controllers/Auth.js";
+import { isLogin } from './../Middlewares/isLogin.js';
 //--
 const router = express.Router();
 //--
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/validation').post(validation);
-router.route('/me').get(getMe);
+router.route('/me').get(isLogin, getMe);
 //--
 export default router
