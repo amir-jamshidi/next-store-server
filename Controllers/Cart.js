e8906ac6e0d14209811a3efc62078b733adbefc8
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-31 15:40:25 +0330

    Feat : Create get Cart

--- Controllers/Cart.js
+++ Controllers/Cart.js
@@ -1,9 +1,27 @@
-export const getCart = async (req, res, next) => {
+import cartModel from '../Models/Cart.js'
 
+export const getCart = async (req, res, next) => {
+    try {
+        const { userID } = req.params;
+        const cart = await cartModel.find({ userID }).populate('productID').lean();
+        if (cart) {
+            res.status(200).json(cart);
+        }
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const insert = async (req, res, next) => {
-
+    try {
+        const { productID, userID, count, total } = req.body;
+        const cart = new cartModel.create({ productID, userID, count, total });
+        if (cart) {
+            res.status(201).json(cart);
+        }
+    } catch (error) {
+        next(error)
+    }
 }
 
 export const remove = async (req, res, next) => {
