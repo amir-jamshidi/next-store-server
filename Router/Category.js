64e41760acf7387ec77ea4b083f0ac58b7a751dd
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-01-13 16:40:49 +0330

    Feat : Compelete getAll Router

--- Router/Category.js
+++ Router/Category.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { get, insert } from '../Controllers/Category.js';
+import { get, getAll, insert } from '../Controllers/Category.js';
 import multer from 'multer';
 import { categoryStorage } from '../Utils/uploader.js';
 import { isLogin } from './../Middlewares/isLogin.js';
@@ -8,5 +8,5 @@ import { isAdmin } from './../Middlewares/isAdmin.js';
 const router = express.Router();
 
 router.route('/').post(isLogin, isAdmin, multer({ storage: categoryStorage }).single('photo'), insert).get(get);
-
+router.route('/all').get(getAll);
 export default router;
