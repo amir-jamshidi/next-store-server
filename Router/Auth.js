74019f1389cbaae1f3da7ceb24aa8fe1554146fd
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-07-27 23:59:58 +0330

    Update Auth.js

--- Router/Auth.js
+++ Router/Auth.js
@@ -5,6 +5,7 @@ import { isLogin } from './../Middlewares/isLogin.js';
 const router = express.Router();
 //--
 
+
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/validation').post(validation);
